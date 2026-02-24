8f4b6e39ce864631d1e7fb653e1c443cf19d0f5b
ibrahimkabadayi <ibrahimkabadayi180@gmail.com>
2026-02-24 18:12:10 +0300

    Integrate event publishing to notify when a new user is created

--- Identity.API/Application/Services/UserService.cs
+++ Identity.API/Application/Services/UserService.cs
@@ -3,13 +3,16 @@ using Identity.API.Application.DTOs;
 using Identity.API.Application.Interfaces;
 using Identity.API.Domain.Entities;
 using Identity.API.Domain.Interfaces;
+using MassTransit;
+using Shared.Messages;
 
 namespace Identity.API.Application.Services;
 
-public class UserService(IUserRepository userRepository, IMapper mapper) : IUserService
+public class UserService(IUserRepository userRepository, IMapper mapper, IPublishEndpoint publishEndpoint) : IUserService
 {
     private readonly IUserRepository _userRepository = userRepository;
     private readonly IMapper _mapper = mapper;
+    private readonly IPublishEndpoint _publishEndpoint = publishEndpoint;
 
     public async Task AddUserAsync(UserDto user)
     {
@@ -17,6 +20,8 @@ public class UserService(IUserRepository userRepository, IMapper mapper) : IUser
         {
             var addUser = _mapper.Map<User>(user);
             await _userRepository.AddAsync(addUser);
+
+            await _publishEndpoint.Publish(new UserCreatedEvent { Email = user.Email, UserName = addUser.Name, UserId = addUser.Id });
         }
         catch (Exception ex) 
         {
