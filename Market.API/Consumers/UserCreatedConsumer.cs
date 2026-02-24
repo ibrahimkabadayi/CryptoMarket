bf7610648b16e1039d74158460324912a3072852
ibrahimkabadayi <ibrahimkabadayi180@gmail.com>
2026-02-24 18:13:08 +0300

    Add `UserCreatedConsumer` to handle `UserCreatedEvent` in Market.API

---
+++ Market.API/Consumers/UserCreatedConsumer.cs
@@ -0,0 +1,17 @@
+﻿using MassTransit;
+using Shared.Messages;
+
+namespace Market.API.Consumers;
+
+public class UserCreatedConsumer : IConsumer<UserCreatedEvent>
+{
+    public async Task Consume(ConsumeContext<UserCreatedEvent> context)
+    {
+        var message = context.Message;
+
+        Console.WriteLine($"\nNew User Added! ID: {message.UserId}, Email: {message.Email}");
+        Console.WriteLine("Creating an empty basket for new user...");
+        
+        await Task.CompletedTask;
+    }
+}
