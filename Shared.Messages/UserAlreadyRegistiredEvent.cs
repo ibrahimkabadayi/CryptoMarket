c93ab22d295c3933c9cc28649f03e50aa48e741f
ibrahimkabadayi <ibrahimkabadayi180@gmail.com>
2026-02-28 15:12:20 +0300

    Shared.Messages address changed

---
+++ Shared.Messages/UserAlreadyRegistiredEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace Shared.Messages;
+
+public record UserAlreadyRegistiredEvent
+{
+    public string UserName { get; set; } = string.Empty;
+    public string UserEmail { get; set; } = string.Empty;
+}
