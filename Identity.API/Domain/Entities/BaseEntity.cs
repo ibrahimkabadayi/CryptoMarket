0e9da696d09d90d7c8ba21ff6e64fa47ee836cc1
ibrahimkabadayi <ibrahimkabadayi180@gmail.com>
2026-03-05 12:43:42 +0300

    Remove unused entities and controllers from Identity.API and Portfolio.API

--- Identity.API/Domain/Entities/BaseEntity.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Identity.API.Domain.Entities;
-
-public abstract class BaseEntity
-{
-    public Guid Id { get; set; } = Guid.NewGuid();
-    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
-    public DateTime? UpdatedDate { get; set; }
-}
--- Identity.API/Domain/Entities/User.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Identity.API.Domain.Entities;
-
-public class User : BaseEntity
-{
-    public string Name { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string PasswordHash { get; set; } = string.Empty;
-}
