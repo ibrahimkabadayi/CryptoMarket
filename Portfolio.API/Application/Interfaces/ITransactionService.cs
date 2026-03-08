512bc3d1b6079b909395bd7a41befcd6f20efe8a
ibrahimkabadayi <ibrahimkabadayi180@gmail.com>
2026-03-08 12:53:42 +0300

    Add CreateTransactionRecordAsync method to ITransactionService and its implementation

--- Portfolio.API/Application/Interfaces/ITransactionService.cs
+++ Portfolio.API/Application/Interfaces/ITransactionService.cs
@@ -1,5 +1,8 @@
-﻿namespace Portfolio.API.Application.Interfaces;
+﻿using Portfolio.API.Domain.Enums;
+
+namespace Portfolio.API.Application.Interfaces;
 
 public interface ITransactionService
 {
+    Task CreateTransactionRecordAsync(Guid walletId, string symbol, double amount, double price, TransactionType type);
 }
--- Portfolio.API/Application/Services/TransactionService.cs
+++ Portfolio.API/Application/Services/TransactionService.cs
@@ -1,7 +1,24 @@
 ﻿using Portfolio.API.Application.Interfaces;
+using Portfolio.API.Domain.Entities;
+using Portfolio.API.Domain.Enums;
+using Portfolio.API.Domain.Interfaces;
+using Portfolio.API.Infrastructure.Repositories;
 
 namespace Portfolio.API.Application.Services;
 
-public class TransactionService : ITransactionService
+public class TransactionService(ITransactionRepository transactionRepository) : ITransactionService
 {
+    public async Task CreateTransactionRecordAsync(Guid walletId, string symbol, double amount, double price, TransactionType type)
+    {
+        var transaction = new Transaction
+        {
+            WalletId = walletId,
+            Symbol = symbol,
+            Amount = amount,
+            PriceAtTransaction = price,
+            TransactionType = type
+        };
+
+        await transactionRepository.AddAsync(transaction);
+    }
 }
