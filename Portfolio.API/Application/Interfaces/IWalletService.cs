bc42bec78cd9cf3416c26493a2e3a3f832d5c2a3
ibrahimkabadayi <ibrahimkabadayi180@gmail.com>
2026-03-08 12:53:13 +0300

    Extend IWalletService with additional wallet operations (deposit, transfer, buy asset)

--- Portfolio.API/Application/Interfaces/IWalletService.cs
+++ Portfolio.API/Application/Interfaces/IWalletService.cs
@@ -1,6 +1,12 @@
-﻿namespace Portfolio.API.Application.Interfaces;
+﻿using Portfolio.API.Application.DTOs;
+
+namespace Portfolio.API.Application.Interfaces;
 
 public interface IWalletService
 {
     Task<string> CreateWallet(Guid UserId);
+    Task<string> DepositMoney(Guid WalletId, double Amount);
+    Task<string> DepositMoney(string WalletAddress, double Amount);
+    Task<string> TransferAsset(TransferAssetDto dto);
+    Task<string> BuyAsset(Guid WalletId, string Symbol, double CurrentPrice, double Amount);
 }
