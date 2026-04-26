a237ddcc3182ae936582aa573c9178d172bbc103
ibrahimkabadayi <ibrahimkabadayi180@gmail.com>
2026-04-26 18:31:30 +0300

    Changed cluster names for Portfolio.API controllers and added Corcs to identify frontend application by gateway service

--- API.Gateway/Program.cs
+++ API.Gateway/Program.cs
@@ -9,10 +9,22 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
         builder.AddServiceTracing("API.Gateway");
 
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("AllowVueApp", policy =>
+            {
+                policy.WithOrigins("http://localhost:5173")
+                      .AllowAnyHeader()
+                      .AllowAnyMethod();
+            });
+        });
+
         builder.Services.AddReverseProxy()
             .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
         var app = builder.Build();
+
+        app.UseCors("AllowVueApp");
 
         app.MapReverseProxy();
 
