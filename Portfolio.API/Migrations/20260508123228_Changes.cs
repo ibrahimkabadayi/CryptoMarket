2ae7dd1a4a914ed7a0ae9f06af7bdf506a53a4ed
ibrahimkabadayi <ibrahimkabadayi180@gmail.com>
2026-05-08 18:46:34 +0300

    Add Migration TreasuryBalance entity

---
+++ Portfolio.API/Migrations/20260508123228_Changes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Portfolio.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Changes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TreasuryBalances",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    AssetSymbol = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    TotalAmount = table.Column<decimal>(type: "numeric(18,8)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TreasuryBalances", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TreasuryBalances");
+        }
+    }
+}
